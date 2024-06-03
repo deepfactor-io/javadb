0b9331fbaefebe83f63e74ec624415eefaa058e8
Akshay Menon <akshay@deepfactor.io>
2024-06-03 14:19:17 +0530

    update parallelism

--- cmd/df-java-db/main.go
+++ cmd/df-java-db/main.go
@@ -79,7 +79,7 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&cacheDir, "cache-dir", filepath.Join(userCacheDir, "df-java-db"),
 		"cache dir")
-	rootCmd.PersistentFlags().IntVar(&limit, "limit", 1000, "max parallelism")
+	rootCmd.PersistentFlags().IntVar(&limit, "limit", 500, "max parallelism")
 
 	rootCmd.AddCommand(crawlCmd)
 	rootCmd.AddCommand(buildCmd)
